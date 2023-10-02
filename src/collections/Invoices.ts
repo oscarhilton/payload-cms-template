027ec3fab60d285ff57371f95ba695499e17efaa
oscarhilton <oscar.hilton@rvu.co.uk>
2023-10-02 18:48:17 +0100

    Add invoices

---
+++ src/collections/Invoices.ts
@@ -0,0 +1,28 @@
+import { CollectionConfig } from 'payload/types';
+import { isAdmin } from "../access"
+
+const Invoices: CollectionConfig = {
+    slug: 'invoices',
+    access: {
+        create: isAdmin,
+        read: isAdmin,
+        update: isAdmin,
+        delete: isAdmin,
+      },
+    fields: [
+        { name: 'invoiceNumber', type: 'text', required: true },
+        { name: 'client', type: 'relationship', relationTo: 'clients', required: true },
+        { name: 'dateIssued', type: 'number', required: true },
+        { name: 'dueDate', type: 'number', required: true },
+        { name: 'totalAmount', type: 'number', required: true },
+        { name: 'status', type: 'select', options: ['Unissued', 'Pending', 'Paid', 'Overdue'], required: true, defaultValue: 'Unissued' },
+        { name: 'items', type: 'array', fields: [
+            { name: 'description', type: 'text', required: true },
+            { name: 'quantity', type: 'number', required: true },
+            { name: 'unitPrice', type: 'number', required: true },
+        ] },
+        { name: 'notes', type: 'textarea' },
+    ],
+};
+
+export default Invoices
